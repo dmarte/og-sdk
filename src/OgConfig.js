148c82321a1c0b60ba4e83f7c6575fc26eb79a1a
Delvi Marte <delvi.marte@gmail.com>
2020-09-18 09:42:45 -0400

    Added get and set methods to OgConfig

--- src/OgConfig.js
+++ src/OgConfig.js
@@ -1,11 +1,30 @@
+import { set, get } from "lodash";
+
 export default class OgConfig {
   constructor() {
     this.$items = {
-      API_URL: ""
+      API_URL: "",
+      API_HEADERS: {}
     };
+
+    this.set("API_HEADERS.Accept", "application/json");
+    this.set("API_HEADERS.Content-Type", "application/json");
+  }
+
+  set(path, value) {
+    set(this.$items, path, value);
+    return this;
+  }
+
+  get(path, defaultValue = null) {
+    return get(this.$items, path, defaultValue);
   }
 
   get API_URL() {
-    return this.$items.API_URL;
+    return this.get("API_URL");
+  }
+
+  get API_HEADERS() {
+    return this.get("API_HEADERS", {});
   }
 }
--- test/OgConfig.test.js
+++ test/OgConfig.test.js
@@ -5,8 +5,16 @@ const config = new OgConfig();
 it("Has default settings", () => {
   expect(config).toBeInstanceOf(OgConfig);
   expect(config.$items).toBeDefined();
+  expect(config.$items).toMatchObject({
+    API_URL: ""
+  });
 });
 
 it("Has app settings getters.", () => {
   expect(config.API_URL).toBeDefined();
+});
+
+it("Setters and getters works.", () => {
+  config.set("APP_NAME", "Demo");
+  expect(config.get("APP_NAME")).toMatch("Demo");
 });
