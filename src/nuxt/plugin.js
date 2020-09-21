68f3070b1dd2fa1a1c31995ff2183061e6526a63
Delvi Marte <delvi.marte@gmail.com>
2020-09-21 10:27:20 -0400

    fix vars problems when nuxt module is used.

--- src/nuxt/plugin.js
+++ src/nuxt/plugin.js
@@ -1,4 +1,4 @@
-import Bootstrap from '~/plugins/sdk/src/Sdk/Bootstrap'
+import Bootstrap from '../Sdk/Bootstrap'
 
 export default (context, inject) => {
   inject('og', new Bootstrap(<%=JSON.stringify(options)%>))
